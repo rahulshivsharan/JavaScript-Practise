8281b0211301dce3b36d4401ec35deb8503afed4
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-08-19 10:31:11 +0530

    Create StudentListApp.js

---
+++ AngularJS/ProAngularJS/modulePractise01/StudentListApp.js
@@ -0,0 +1,47 @@
+angular.module("StudentListApp").controller("StudentListCntrl",function($scope){
+	$scope.data = [
+		{
+			name : "John Grisham",
+			major : "Computer Graphics",
+			College : "Texas University",
+			Grade : "A"
+		},
+		{
+			name : "Suzane Abignale",
+			major : "Sociology",
+			College : "Kellogs University",
+			Grade : "B"
+		},
+		{
+			name : "Timothy Simsons",
+			major : "Anthropology",
+			College : "Nixxon Trust of Education",
+			Grade : "B"
+		},
+		{
+			name : "Rick Sullivan",
+			major : "Software Engineering",
+			College : "Masachussate Institute of Technology",
+			Grade : "A"
+		},
+		{
+			name : "Nathan Gonzales",
+			major : "International Business",
+			College : "Cambridge University",
+			Grade : "A"
+		},
+		{
+			name : "Ridley Scott",
+			major : "Computer Animation",
+			College : "Masachussate Institute of Technology",
+			Grade : "A"
+		},
+		{
+			name : "Jack Nicholson",
+			major : "Market Grading and Statistics",
+			College : "Cambridge University",
+			Grade : "A"
+		}
+	];
+	
+});	
