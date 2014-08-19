3152e492253cb3f3b25d2f84baf3ad9cb788c42b
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-08-19 18:30:19 +0530

    Inital creation of Module and Controller
    Pre-creation of Module in external JS file, which also has Controller

---
+++ AngularJS/ProAngularJS/multi-Controller/StudentListApp.js
@@ -0,0 +1,54 @@
+angular.module("StudentListApp").controller("StudentListCntrl",function($scope){
+	$scope.data = [
+		{
+			id : 123,
+			name : "John Grisham",
+			major : "Computer Graphics",
+			College : "Texas University",
+			Grade : "A"
+		},
+		{
+			id : 124,
+			name : "Suzane Abignale",
+			major : "Sociology",
+			College : "Kellogs University",
+			Grade : "B"
+		},
+		{
+			id : 125,
+			name : "Timothy Simsons",
+			major : "Anthropology",
+			College : "Nixxon Trust of Education",
+			Grade : "B"
+		},
+		{
+			id : 126,
+			name : "Rick Sullivan",
+			major : "Software Engineering",
+			College : "Masachussate Institute of Technology",
+			Grade : "A"
+		},
+		{
+			id : 127,
+			name : "Nathan Gonzales",
+			major : "International Business",
+			College : "Cambridge University",
+			Grade : "A"
+		},
+		{
+			id : 128,
+			name : "Ridley Scott",
+			major : "Computer Animation",
+			College : "Masachussate Institute of Technology",
+			Grade : "A"
+		},
+		{
+			id : 129,
+			name : "Jack Nicholson",
+			major : "Market Grading and Statistics",
+			College : "Cambridge University",
+			Grade : "A"
+		}
+	];
+	
+});	
