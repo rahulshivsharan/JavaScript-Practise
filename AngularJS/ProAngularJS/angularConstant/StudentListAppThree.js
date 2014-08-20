b4d29ff9c5a4eabe32d0f1b0322c09a2d990a847
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-08-20 14:51:18 +0530

    Create StudentListAppThree.js

---
+++ AngularJS/ProAngularJS/angularConstant/StudentListAppThree.js
@@ -0,0 +1,60 @@
+angular.module("StudentListApp")
+		.constant("showGradeBClass","showAltert")
+		.constant("showGradeAClass","showSuccess")
+		.controller("StudentListCntrl",function($scope,showGradeBClass,showGradeAClass){
+	$scope.data = [
+		{
+			id : 123,
+			name : "John Grisham",
+			major : "Computer Graphics",
+			College : "Texas University",
+			Grade : "A"
+		},
+		{
+			id : 124,
+			name : "Suzane Abignale",
+			major : "Sociology",
+			College : "Kellogs University",
+			Grade : "B"
+		},
+		{
+			id : 125,
+			name : "Timothy Simsons",
+			major : "Anthropology",
+			College : "Nixxon Trust of Education",
+			Grade : "B"
+		},
+		{
+			id : 126,
+			name : "Rick Sullivan",
+			major : "Software Engineering",
+			College : "Masachussate Institute of Technology",
+			Grade : "A"
+		},
+		{
+			id : 127,
+			name : "Nathan Gonzales",
+			major : "International Business",
+			College : "Cambridge University",
+			Grade : "A"
+		},
+		{
+			id : 128,
+			name : "Ridley Scott",
+			major : "Computer Animation",
+			College : "Masachussate Institute of Technology",
+			Grade : "A"
+		},
+		{
+			id : 129,
+			name : "Jack Nicholson",
+			major : "Market Grading and Statistics",
+			College : "Cambridge University",
+			Grade : "A"
+		}
+	];
+	
+	$scope.gradClass = function(student){
+		return (student.Grade == "B") ? showGradeBClass : showGradeAClass;
+	}
+});	
