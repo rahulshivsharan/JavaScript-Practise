b762fc5774f00fdda4781c08ffd9d2bce15bd6b0
Rahul Shivsharan <postbox.rahul@hotmail.com>
2015-09-07 18:56:37 +0530

    Singleton patterns Implimented

--- web/design-pattern/singleton-01.js
+++ web/design-pattern/singleton-01.js
@@ -1,3 +1,33 @@
+var fn86 = function(){
+		var Student = {};
+		
+		(function(){
+			var obj = new Object();
+			
+			this.getInstance = function(){
+				return obj;
+			};
+			
+		}).apply(Student);
+		
+		var obj1 = Student.getInstance();
+		obj1.name = "Rahul Shivsharan";
+		
+		var obj2 = Student.getInstance();
+		
+		print("obj1.name "+obj1.name); // obj1.name Rahul Shivsharan
+		print("obj2.name "+obj2.name); // obj2.name Rahul Shivsharan
+		obj2.name = "Ninad Kulkarni";  
+		print("obj1.name "+obj1.name); // obj1.name Ninad Kulkarni
+		
+		var obj3 = Student.getInstance();
+		print("obj3.name "+obj1.name); // obj3.name Ninad Kulkarni
+};
+
+//fn86();
+
+
+// Below is the more imporved singleton
 var fn70 = function(){
 		var Student = (function(){
 			var obj = undefined;
