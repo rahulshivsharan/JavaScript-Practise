99a9d9dc3117cf2920b243a2e2a6fd916af593b4
Rahul Shivsharan <postbox.rahul@hotmail.com>
2017-07-07 11:37:04 +0530

    Binary Search algorithm using array copy
    This binary search algorithm implementation is done by using a facility of array copy in JavaScript.
    So by copying the sub array in the new array and passing the same copied array in the recursive call of binarySearch method
    as an argument is the technique.

---
+++ dataStructures/binarySearch02.js
@@ -0,0 +1,67 @@
+var myArray = [], 
+	searchNum = undefined;
+
+// below function is used to capture the 
+// commandline parameters for array and the
+// number to be searched
+(function(){
+	process.argv.forEach(function (val, index, array) {
+		var idx = 0, ar = undefined;
+
+		try{
+			
+			// get the commandline argument for 
+			// array values 
+			if(index === 2){
+  				myArray = myArray.concat(val.split(",").map(function(num){
+  					return parseInt(num);
+  				}));	
+			}
+
+			// third index is the number to be searched.
+			if(index === 3){
+				searchNum = parseInt(val)
+			}
+
+		}catch(e){
+			console.log(e)
+		}
+		
+	});
+})();
+
+console.log(" SEARCH NUMBER ",searchNum," in array ",myArray);
+//console.log(" Number ",searchNum," "+binarySearch(myArray,searchNum));
+
+// binary-Search implementation
+function binarySearch(myArray,numberToSearch){
+	var arrayLength = myArray.length,
+		midIndex = parseInt(arrayLength/2), 
+		subArray = undefined,
+		lowerIndex = 0,
+		higherIndex = 0; 
+	
+	if(myArray[midIndex] === numberToSearch){
+		return "is Found";
+
+	// search the number in left side if array
+	// i.e. number is found to the left of the 
+	// middle Index	
+	}else if(midIndex !== 0 && myArray[midIndex] > numberToSearch){ 
+		lowerIndex = 0;
+		higherIndex = midIndex;
+		subArray = myArray.slice(lowerIndex,higherIndex); // create the sub-array
+		return binarySearch(subArray,numberToSearch); // search the number in the subarray
+
+	// search the number in right side if array
+	// i.e. number is found to the right of the 
+	// middle Index	
+	}else if(midIndex !== 0 && myArray[midIndex] < numberToSearch){ // search the number in right side if array
+		lowerIndex = midIndex + 1;
+		higherIndex = arrayLength;
+		subArray = myArray.slice(lowerIndex,higherIndex); // create the sub-array
+		return binarySearch(subArray,numberToSearch); // search the number in the subarray
+	}else{
+		return "can't be found";
+	}	
+} // end of method binarySearch
